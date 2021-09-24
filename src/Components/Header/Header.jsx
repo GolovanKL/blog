9dd63125a55d9e3ba9ee16d0ae01f5c7f5f66f89
Konstantin Golovan <mindrot@yandex.ru>
2021-09-24 16:51:12 +0300

    Second stage: finished validation forms sign-in, sign-out with react-hook-form

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -8,12 +8,14 @@ const MyComponent = () => {
   return (
     <header className="header">
       <div className="header__title">
-        <h6>Realworld Blog</h6>
+        <Link to={'/'}>
+          <h6>Realworld Blog</h6>
+        </Link>
       </div>
       <div className="header__auth auth">
         <div className="auth__sign-in auth__element">
           <Link to={'/sign-in'}>Sign In</Link>
-          </div>
+        </div>
         <div className="auth__sign-up auth__element">
           <Link to={'/sign-up'}>Sign Up</Link>
         </div>
